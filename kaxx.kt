ae38f9e90e91f0261a4b3ce155f01ca5f1fa3e9a
Anyijukire <79198971+Anyijukire@users.noreply.github.com>
2021-05-16 12:02:27 +0300

    Update kaxx.kt

--- kaxx.kt
+++ kaxx.kt
@@ -4,9 +4,10 @@ fun main() {
     println(isEven(66))
     println( myFunction("carrot"))
     println(myFunction("maize"))
-    var category1=Product("apples ",5,600,"food")
+    println(myFunction("banana"))
+    var category1=Product("apples ",5,600,"gloceries")
     println(category1)
-    var category2=Product("drugs",2,400,"chemical")
+    var category2=Product("drugs",2,400,"hygien")
     println(category2)
     var category3=Product("knife",1,200,"other")
     println(category3)
@@ -16,20 +17,24 @@ fun main() {
 fun isEven(x: Int): Boolean {
     if (x % 2 == 0) {
         return true
-    } else
-        return false
+    }
+    return false
+    //return x%2==0
 }
 data class Product( var name:String, var weight:Int,var price:Int,var category:String) {
-    fun product(name:String,weight:Int,price:Int,category:Int) {
-        var gloceryList = mutableListOf<String>()
-        var hygieneList = mutableListOf<String>()
-        var otherList = mutableListOf<String>()
+    fun product(name:Product) {
+        var gloceryList = mutableListOf<Product>()
+        var hygieneList = mutableListOf<Product>()
+        var otherList = mutableListOf<Product>()
         when ("category") {
             "gloceries"  -> println( gloceryList.add(name))
             "hygiene" -> println(hygieneList.add(name))
             else-> println(otherList.add(name))
         }
-        println("${name}:${category}")
+       // println("${name}:${category}")
+        println(gloceryList)
+        println(hygieneList)
+        println(otherList)
     }
 }
 
